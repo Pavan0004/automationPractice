5383350ea9f1c1df4ad17fd347e8eddc6c582b0d
Pavan0004 <manganellorepavan5@gmail.com>
2023-03-17 10:36:02 +0530

    Created New Project

---
+++ seleniumPractice/src/main/java/com/selenium/practice/com/selenium/practice/App.java
@@ -0,0 +1,8 @@
+package com.selenium.practice.com.selenium.practice;
+
+public class App {
+
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello World!");
+    }
+}
